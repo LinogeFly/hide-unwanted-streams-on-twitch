aabe0bf44082fd1b4881ae4fccf3c262bdbcc93c
LinogeFly <LinogeFly@users.noreply.github.com>
2015-01-31 20:46:03 +0200

    Added logging support for debug build only.

--- src/scripts/log.js
+++ src/scripts/log.js
@@ -10,3 +10,11 @@ husot.log.info = function (obj) {
 husot.log.error = function (obj) {
     console.log('%cHUSOT: ' + obj, 'color: #ff0000');
 };
+
+husot.log.debug = function (obj) {
+    if (typeof husot.debug === 'undefined') {
+        return;
+    }
+
+    console.log('HUSOT DEBUG: ' + obj);
+}
