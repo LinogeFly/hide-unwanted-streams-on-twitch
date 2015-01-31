37472f46e8161c29ed363531e9b427993922d319
LinogeFly <LinogeFly@users.noreply.github.com>
2015-02-01 01:50:49 +0200

    Fixed a bug (hopefully) when "infinite scroll" was not triggered sometimes.

--- src/scripts/injects/loadMoreThumbs.js
+++ src/scripts/injects/loadMoreThumbs.js
@@ -1,7 +1,7 @@
 // Loads more stream/video thumbnails by calling internal function of Twitch
 document.addEventListener('husot.loadMoreThumbs', function (e) {
     var thumbsView = Ember.View.views[$('#directory-list .items > .ember-view').attr('id')];
-    if (typeof thumbsView === 'undefined' || typeof thumbsView.content === 'undefined') {
+    if (typeof thumbsView === 'undefined') {
         return;
     };
 
