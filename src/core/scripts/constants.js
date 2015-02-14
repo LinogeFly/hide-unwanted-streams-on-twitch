a065fdd14505455fc5654cdf00baa79042845aac
LinogeFly <LinogeFly@users.noreply.github.com>
2015-02-14 20:57:50 +0200

    Optimized Allowed URLs regular expressions.

--- src/core/scripts/constants.js
+++ src/core/scripts/constants.js
@@ -10,8 +10,7 @@ husot.constants.blockedGamesListEmpty = 'No Blocked Games';
 husot.constants.modalDialogShowingSpeed = 150;
 husot.constants.allowedUrls = [
     '^http://www.twitch.tv/directory/?$',
-    '^http://www.twitch.tv/directory/all/?$',
-    '^http://www.twitch.tv/directory/all/.+',
+    '^http://www.twitch.tv/directory/all(/?|/.+)$',
     '^http://www.twitch.tv/directory/game/.+',
     '^http://www.twitch.tv/directory/random/?$',
     '^http://www.twitch.tv/directory/videos/.+'
