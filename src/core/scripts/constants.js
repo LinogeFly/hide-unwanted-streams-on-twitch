ec10f067cf3c1c34222e58bb27e9bd7012bc4d62
LinogeFly <LinogeFly@users.noreply.github.com>
2017-02-18 16:29:36 +0200

    WIP.

--- src/core/scripts/constants.js
+++ src/core/scripts/constants.js
@@ -13,7 +13,8 @@ husot.constants.allowedUrls = [
     '^https?://([a-zA-Z]+\.)?twitch.tv/directory/all(/?|/.+)$',
     '^https?://([a-zA-Z]+\.)?twitch.tv/directory/game/.+',
     '^https?://([a-zA-Z]+\.)?twitch.tv/directory/random/?$',
-    '^https?://([a-zA-Z]+\.)?twitch.tv/directory/videos/.+'
+    '^https?://([a-zA-Z]+\.)?twitch.tv/directory/videos/.+',
+    '^https?://([a-zA-Z]+\.)?twitch.tv/communities/.+', // TODO: this has to be more accurate.
 ];
 husot.constants.blockedItemType = husot.constants.blockedItemType || {};
 husot.constants.blockedItemType.game = 'game';
